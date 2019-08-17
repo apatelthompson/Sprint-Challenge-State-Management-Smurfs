b0c6db9160380145b68d7eb84af3d9360d22bc0d
avni <avni@hellorosey.com>
2019-08-17 11:19:57 -0700

    working on fixing post

--- smurfs/src/actions/postSmurfActions.js
+++ smurfs/src/actions/postSmurfActions.js
@@ -4,9 +4,9 @@ export const POST_SMURF_DATA_START = "POST_SMURF_DATA_START";
 export const POST_SMURF_DATA_SUCCESS = "POST_SMURF_DATA_SUCCESS";
 export const POST_SMURF_DATA_FAILURE = "POST_SMURF_DATA_FAILURE";
 
-export const postSmurfData = () => dispatch => {
+export const postSmurfData = values => dispatch => {
   axios
-    .post("http://localhost:3333/smurfs")
+    .post("http://localhost:3333/smurfs", { data: values })
     .then(response => {
       dispatch({ type: POST_SMURF_DATA_SUCCESS, payload: response });
     })
